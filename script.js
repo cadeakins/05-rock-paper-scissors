b3ea3f39187ace1ea260f2c16307f9c6cee607bb
Cade Medearis <108916110+cadeakins@users.noreply.github.com>
2025-06-27 03:03:46 +0000

    Completed assignment.

--- script.js
+++ script.js
@@ -5,15 +5,61 @@ const scissorsButton = document.getElementById("scissors");
 
 // Add event listener for rock button
 rockButton.addEventListener("click", function() {
-  console.log("You chose: Rock 🪨");
+  playGame("Rock");
 });
 
 // Add event listener for paper button
 paperButton.addEventListener("click", function() {
-  console.log("You chose: Paper 📄");
+  playGame("Paper");
 });
 
 // Add event listener for scissors button
 scissorsButton.addEventListener("click", function() {
-  console.log("You chose: Scissors ✂️");
+  playGame("Scissors");
 });
+
+
+
+function getComputerChoice() {
+  //Generate a random choice for the computer
+  const randomNumber = Math.floor(Math.random() * 3);
+
+  if (randomNumber === 0) {
+    return "Rock";
+  }
+  if (randomNumber === 1) {
+    return "Paper";
+  }
+  
+  return "Scissors";
+}
+
+function playGame(playerChoice) {
+  //Get the computer's choice
+  const computerChoice = getComputerChoice();
+
+  let message = `You chose: ${playerChoice} <br> Computer chose: ${computerChoice}`;
+
+  let result = "";
+
+  //Tie check
+  if(playerChoice === computerChoice) {
+    result = "It's a tie!";
+  } else if (
+    (playerChoice === "Rock" && computerChoice === "Scissors") ||
+    (playerChoice === "Paper" && computerChoice === "Rock") ||
+    (playerChoice === "Scissors" && computerChoice === "Paper")
+  ) {
+    result = "You win!";
+  } else {
+    result = "Computer wins!";
+  }
+
+  //Add result to the message
+  message = `${message}<br>${result}`;
+
+  //Show message on page
+  const resultDiv = document.getElementById("result");
+
+  resultDiv.innerHTML = message;
+}
